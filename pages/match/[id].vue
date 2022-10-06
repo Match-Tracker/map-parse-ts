8dfb427bff2f7296c95cdbeee668b57fb233d6c6
Gobbo <yutubemoby@gmail.com>
2022-10-06 03:20:04 +0100

    refactor: :recycle: imports & styles

--- pages/match/[id].vue
+++ pages/match/[id].vue
@@ -1,42 +1,48 @@
 <template>
-	<div class="py-12">
-		<div class="overview flex flex-row min-w-full justify-center items-start gap-12">
+	<div>
+		<div v-show="!hasLoaded" class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+			<div class="btn btn-ghost btn-lg loading" />
+			<p class="text-white mt-1">Loading Match overview...</p>
+		</div>
+		<div v-if="hasLoaded" class="overview flex flex-row min-w-full justify-center items-start gap-12">
 			<div v-if="match.canvas" class="player__menus flex gap-3">
-				<PlayerTab :showRed="true" :showBlue="true" @update="playerChange" />
+				<PlayersPlayerTab :showRed="true" :showBlue="true" @update="playerChange" />
 			</div>
 
 
 			<div class="flex flex-col w-1/3 h-1/3">
-				<MapView1 v-if="match.metadata.matchid" />
+				<MatchOverview class="mx-auto mb-6" />
+
+				<MapView v-if="match.metadata.matchid" />
 				<!-- <KillView /> -->
 
-				<RangeSlide @update="rangeUpdate" />
+				<RangeSlide class="mt-4" @update="rangeUpdate" />
 			</div>
 
-			<FilterOptions />
+			<FiltersFilterOptions />
 		</div>
 	</div>
 </template>
 
 
 <script lang="ts" setup>
-import PlayerTab from '~~/components/Players/PlayerTab.vue';
 import RangeSlide from '~~/components/Rounds/RangeSlide.vue';
 
 import type { Player } from '@/types/match';
 import useMatch from '~~/store/match';
-import MapView1 from '../../components/Map/MapView.vue';
-import FilterOptions from '~~/components/Filters/FilterOptions.vue';
+import { Ref } from 'vue';
 
 const matchId: string = useRoute().params.id as string;
-
 const match = useMatch();
+const hasLoaded: Ref<boolean> = ref(false);
 
 let timeRange: number[] = [0, 300];
 let curPlayer: Player | null = null;
 
 onMounted(async () => {
 	await match.fetchMatch(matchId);
+
+	hasLoaded.value = true;
 })
 
 function rangeUpdate(range: number[]) {
@@ -62,3 +68,12 @@ function draw(curPlayer: Player | null, timeRange: number[]) {
 	}
 }
 </script>
+
+<style scoped>
+.btn.loading.btn-lg:before {
+	height: 4rem;
+	width: 4rem;
+	border-width: 6px;
+	animation: spin 1s linear infinite;
+}
+</style>
