ffaaa40a465caeba9b6015706cd73466a28f2632
Gobbo <yutubemoby@gmail.com>
2022-10-06 03:18:34 +0100

    fix: :bug: Triangle fix & refactor

--- composables/map.ts
+++ composables/map.ts
@@ -116,7 +116,47 @@ class Map {
 		return viewRadian - Math.PI / 2;
 	}
 
-	
+	getPlayerFromPuuid(puuid: string) {
+		return this.match.players.all_players.find((player) => player.puuid === puuid) || this.match.players.all_players[0];
+	}
+
+	fetchAgent (player: Player): string {
+		const agentId: string = player.assets.agent.small.split('/')[4].toUpperCase() || '';
+		
+		return AgentID[agentId as keyof typeof AgentID] || 'unknown';
+	}
+
+	drawPlayerCircle (x: number, y: number, radius: number, player: Player, victim?: boolean) {
+		this.ctx.beginPath();
+		this.ctx.arc(x, y, radius, 0, Math.PI * 2);
+		this.ctx.fillStyle = `hsla(${player.color}, ${victim ? 0.5 : 1})`
+		this.ctx.fill();
+		this.ctx.strokeStyle = 'hsla(0, 0, 0, 1)'
+		this.ctx.stroke()
+		this.ctx.closePath();
+	}
+
+	drawAgent (x: number, y: number, radius: number, player: Player, victim?: boolean) {
+		// Function that grabs the Agent image and applies it to the canvas as a circle thumbnail
+		const agent: string = this.fetchAgent(player);
+
+		// Mask
+		this.ctx.save();
+		
+		this.ctx.beginPath();
+			this.ctx.fillStyle = 'rgba(0, 0, 0, 0)';
+				this.ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+				this.ctx.fill();
+		this.ctx.closePath();
+		this.ctx.clip();
+
+		this.ctx.drawImage(this.imageCache[agent], x - radius, y - radius, radius * 2, radius * 2);
+
+		// Draw a circle around the agent to show their team
+		this.drawPlayerCircle(x, y, radius, player, victim);
+		this.ctx.restore();
+	}
+
 	drawPlayerPositions(round: number, kill_index: number) {
 		console.log('Drawing player positions for round', round, 'and kill index', kill_index);
 		const kills: Kill[] = this.match.kills;
@@ -160,86 +200,28 @@ class Map {
 		});
 	}
 
-	drawLine(x1: number, y1: number, x2: number, y2: number, player: Player) {
-		
-		const { x: canvas_x1, y: canvas_y1 } = this.calculateLocation(x1, y1);
-		const { x: canvas_x2, y: canvas_y2 } = this.calculateLocation(x2, y2);
-		
-		// Draw the line from the killer to the victim with a blue color with a thickness of 5
-		this.ctx.beginPath();
-		this.ctx.strokeStyle = `hsl(${player.color})`;
-		this.ctx.moveTo(canvas_x1, canvas_y1);
-		this.ctx.lineTo(canvas_x2, canvas_y2);
-		this.ctx.stroke();
-		this.ctx.closePath();
-	}
-
 	drawPlayer(x: number, y: number, radius: number, player: Player, direction?: number, agent?: boolean, victim?: boolean) {
 		
 		const { x: canvas_x, y: canvas_y } = this.calculateLocation(x, y);
 		
-		// Calulate their radians and draw a small triangle outside of the circle to show their direction
-		if (direction) {
-			const radians = this.calculateRadians(direction);
-
-			const triangle_x = canvas_x + Math.cos(radians) * (radius + 2);
-			const triangle_y = canvas_y + Math.sin(radians) * (radius + 2);
-
-			this.drawTriangle(triangle_x, triangle_y, 6, radians, player);
-		}
-		
 		if (agent) {
-			this.drawAgent(canvas_x, canvas_y, radius * 2, player, victim);
+			this.drawAgent(canvas_x, canvas_y, radius, player, victim);
 		} else {
 			this.drawPlayerCircle(canvas_x, canvas_y, radius, player, victim);
 		}
 
+		// Calulate their radians and draw a small triangle outside of the circle to show their direction
+		if (direction) {
+			const radians = this.calculateRadians(direction);
+
+			const triangle_x = canvas_x + Math.cos(radians) * (radius);
+			const triangle_y = canvas_y + Math.sin(radians) * (radius);
+
+			this.drawTriangle(triangle_x, triangle_y, radius, radians, player);
+		}
 	}
 
-	drawTriangle(x: number, y: number, radius: number, radians: number, player: Player) {
-		this.ctx.beginPath();
-		this.ctx.fillStyle = `hsl(${player.color})`;
-		this.ctx.strokeStyle = 'hsla(0, 0, 0, 1)'
-		this.ctx.moveTo(x + Math.cos(radians) * radius, y + Math.sin(radians) * radius);
-		this.ctx.lineTo(x + Math.cos(radians + (2 * Math.PI) / 3) * radius, y + Math.sin(radians + (2 * Math.PI) / 3) * radius);
-		this.ctx.lineTo(x + Math.cos(radians + (4 * Math.PI) / 3) * radius, y + Math.sin(radians + (4 * Math.PI) / 3) * radius);
-		this.ctx.fill();
-		this.ctx.closePath();
-		this.ctx.stroke();
-	}
-
-	drawPlayerCircle (x: number, y: number, radius: number, player: Player, victim?: boolean) {
-		this.ctx.beginPath();
-		this.ctx.arc(x, y, radius, 0, Math.PI * 2);
-		this.ctx.fillStyle = `hsla(${player.color}, ${victim ? 0.5 : 1})`
-		this.ctx.fill();
-		this.ctx.strokeStyle = 'hsla(0, 0, 0, 1)'
-		this.ctx.stroke()
-		this.ctx.closePath();
-	}
-
-	drawAgent (x: number, y: number, radius: number, player: Player, victim?: boolean) {
-		// Function that grabs the Agent image and applies it to the canvas as a circle thumbnail
-		const agent: string = this.fetchAgent(player);
-
-		// Mask
-		this.ctx.save();
-		
-		this.ctx.beginPath();
-			this.ctx.fillStyle = 'rgba(0, 0, 0, 0)';
-				this.ctx.arc(x, y, radius / 2, 0, Math.PI * 2, true);
-				this.ctx.fill();
-		this.ctx.closePath();
-		this.ctx.clip();
-
-		this.ctx.drawImage(this.imageCache[agent], x - radius / 2, y - radius / 2, radius, radius);
-
-		// Draw a circle around the agent to show their team
-		this.drawPlayerCircle(x, y, radius / 2, player, victim);
-		this.ctx.restore();
-	}
-
-	drawMovementMap (player: Player, timeRange: number[],  drawAgents: boolean) {
+	drawMovementMap (player: Player, timeRange: number[], drawAgents: boolean) {
 
 		// Map out all players movements from every kill
 		const kills: Kill[] = this.match.kills.filter((kill) => kill.kill_time_in_round / 1000 >= timeRange[0] && kill.kill_time_in_round / 1000 <= timeRange[1]);
@@ -259,14 +241,30 @@ class Map {
 		});
 	}
 
-	getPlayerFromPuuid(puuid: string) {
-		return this.match.players.all_players.find((player) => player.puuid === puuid) || this.match.players.all_players[0];
+	drawLine(x1: number, y1: number, x2: number, y2: number, player: Player) {
+		
+		const { x: canvas_x1, y: canvas_y1 } = this.calculateLocation(x1, y1);
+		const { x: canvas_x2, y: canvas_y2 } = this.calculateLocation(x2, y2);
+		
+		// Draw the line from the killer to the victim with a blue color with a thickness of 5
+		this.ctx.beginPath();
+		this.ctx.strokeStyle = `hsl(${player.color})`;
+		this.ctx.moveTo(canvas_x1, canvas_y1);
+		this.ctx.lineTo(canvas_x2, canvas_y2);
+		this.ctx.stroke();
+		this.ctx.closePath();
 	}
 
-	fetchAgent (player: Player): string {
-		const agentId: string = player.assets.agent.small.split('/')[4].toUpperCase() || '';
-		
-		return AgentID[agentId as keyof typeof AgentID] || 'unknown';
+	drawTriangle(x: number, y: number, radius: number, radians: number, player: Player) {
+		this.ctx.beginPath();
+		this.ctx.fillStyle = `hsl(${player.color})`;
+		this.ctx.strokeStyle = 'hsla(0, 0, 0, 1)'
+		this.ctx.moveTo(x + Math.cos(radians + (2 * Math.PI) / 3) * radius, y + Math.sin(radians + (2 * Math.PI) / 3) * radius);
+		this.ctx.lineTo(x + Math.cos(radians) * radius, y + Math.sin(radians) * radius);
+		this.ctx.lineTo(x + Math.cos(radians + (4 * Math.PI) / 3) * radius, y + Math.sin(radians + (4 * Math.PI) / 3) * radius);
+		this.ctx.fill();
+		this.ctx.stroke();
+		this.ctx.closePath();
 	}
 }
 
