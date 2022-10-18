7c29ef8315f39e6ec073a5d1ba1d18659e8bc096
KayleeWilliams <65376239+KayleeWilliams@users.noreply.github.com>
2022-10-18 21:34:13 +0100

    feat: :sparkles: Pistol Round Wins & Plant Counters

--- components/Match/MatchStats.vue
+++ components/Match/MatchStats.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col justify-center overflow-visible gap-4">
-    <p class="text-lg font-bold text-center"> Match Stats </p>
+    <p class="text-2xl font-bold text-center"> Match Stats </p>
 
     <div class="tabs tabs-boxed w-fit mx-auto">
 			<a :class="`tab ${isActive('All') ? 'tab-active' : ''}`" @click="setActive('All')">All</a>
@@ -9,14 +9,14 @@
     </div>
 
     <div class="flex w-full">
-      <div class="flex flex-col card bg-base-300 rounded-box w-1/2 justify-center items-center p-8 gap-2">
-        <p> Team 1</p>
-        <StatBox v-for="(stat) in stats.stats" :title="stat['title']" :value="stat['Blue']" />
+      <div class="flex flex-col card bg-base-300 rounded-box w-1/2 justify-start items-center p-8 gap-2 border-blue-500 border-2">
+        <p class="font-bold text-xl"> Team 1 </p>
+        <div class="flex flex-row gap-2 flex-wrap"> <StatBox v-for="(stat) in stats.stats" :title="stat['title']" :value="stat['Blue']" /> </div>
       </div>
       <div class="divider divider-horizontal"></div>
-      <div class="flex flex-col card bg-base-300 rounded-box w-1/2 justify-center items-center p-8 gap-2 h-80">
-        <p> Team 2</p>
-        <StatBox v-for="(stat) in stats.stats" :title="stat['title']" :value="stat['Red']" />
+      <div class="flex flex-col card bg-base-300 rounded-box w-1/2 justify-start items-center p-8 gap-2 h-80 border-red-500 border-2">
+        <p class="font-bold text-xl"> Team 2 </p>
+        <div class="flex flex-row gap-2 flex-wrap"> <StatBox v-for="(stat) in stats.stats" :title="stat['title']" :value="stat['Red']" /> </div>
       </div>    
     </div>
   </div>
--- composables/match-stats.ts
+++ composables/match-stats.ts
@@ -7,10 +7,6 @@ export enum Side {
 	Defence = 'Defence'
 }
 
-export enum Team {
-	Blue = 0,
-	Red = 0
-}
 
 class MatchStats {
   side: Side;
@@ -18,7 +14,14 @@ class MatchStats {
 
   constructor() {
     this.side = Side.All;
-		this.stats = [{title: 'Operator Rounds %', blue: '', red: ''}];
+		this.stats = [
+			{title: 'Operator Rounds %', blue: '', red: ''},
+			{title: 'Pistol Rounds Wins', blue: '', red: ''},
+			{title: 'A Site Plants', blue: '', red: ''},
+			{title: 'B Site Plants', blue: '', red: ''},
+			{title: 'C Site Plants', blue: '', red: ''},
+
+	];
   }
 	
   newSide(tab: Side) {
@@ -57,10 +60,45 @@ class MatchStats {
 		this.stats.find((stat) => stat['title'] === 'Operator Rounds %')['Red'] = `${((redRounds / match.rounds.length) * 100).toFixed(1)}%`
 	}
 
+	pistolRoundsWon(match) {
+		const pistolRounds = [match.rounds[0].winning_team,  match.rounds[12].winning_team];
+		const roundScore = [0, 0]
+		pistolRounds.forEach((round) => {
+			if (round === 'Blue') {
+				roundScore[0] += 1
+			} else {
+				roundScore[1] += 1
+			}
+		})
+
+		this.stats.find((stat) => stat['title'] === 'Pistol Rounds Wins')['Blue'] = `${((roundScore[0] / pistolRounds.length) * 100).toFixed(1)}%`		
+		this.stats.find((stat) => stat['title'] === 'Pistol Rounds Wins')['Red'] = `${((roundScore[1] / pistolRounds.length) * 100).toFixed(1)}%`		
+	}
+
+	plantSites(match) {
+		const planted = [{A: 0, B: 0, C: 0}, {A: 0, B: 0, C: 0},]
+		match.rounds.forEach((round, roundIndex) => {
+			planted.forEach((team, teamIndex) => {
+				if (((teamIndex === 0 && this.isAttack('Blue', Side.Attack, roundIndex)) || (teamIndex === 1 && this.isAttack('Red', Side.Attack, roundIndex)))  && round.plant_events.plant_site != null) {
+					team[round.plant_events.plant_site] += 1
+				}
+			})
+		})
+		
+		const sites = ['A', 'B', 'C'].forEach((site) => {
+					this.stats.find((stat) => stat['title'] === `${site} Site Plants`)['Blue'] = `${planted[0][site]}`		
+					this.stats.find((stat) => stat['title'] === `${site} Site Plants`)['Red'] = `${planted[1][site]}`		
+		})
+
+		console.log(sites)
+	}
+
 	update() {
-		const match = useMatch()
-		this.operatorRounds(match)
-		return this.stats
+		const match = useMatch();
+		this.operatorRounds(match);
+		this.pistolRoundsWon(match);
+		this.plantSites(match);
+		return this.stats;
 	}
 }
 
